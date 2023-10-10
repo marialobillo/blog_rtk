c181377e3af316841b29e7731e812454c022cc32
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-10-10 11:52:07 +0200

    feat: add model for author

---
+++ src/models/Author.ts
@@ -0,0 +1,18 @@
+import { Schema, model } from "mongoose";
+import { IAuthor } from "../interfaces/author.interface";
+
+const AuthorSchema: Schema = new Schema(
+    {
+        fullName: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, require: true, minLength: 6, maxLength: 255 },
+    },
+    {
+        timestamps: true,
+        versionKey: false,
+    }
+);
+
+const AuthorModel = model<IAuthor>("Author", AuthorSchema);
+
+export default AuthorModel;
