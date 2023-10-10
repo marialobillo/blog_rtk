3c00a1c7720e9b17842c2c0c4afdfdcfd1dc0c18
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-10-10 13:52:40 +0200

    feat: add routes for auth: login and register authors

---
+++ src/routes/auth.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express'
+import { registerController, loginController } from '../controllers/auth'
+
+const router = Router()
+
+router.post('/register', registerController)
+router.post('/login', loginController)
+
+export { router }
---
+++ src/routes/index.ts
@@ -0,0 +1,20 @@
+import { Router } from 'express'
+import { readdirSync } from 'fs'
+
+const PATH_ROUTER = `${__dirname}/`
+const router = Router()
+
+const cleanFileName = (fileName: string) => {
+    const file = fileName.split('.').shift()
+    return file ? file : ''
+}
+
+readdirSync(PATH_ROUTER).filter(async (fileName) => {
+    const cleanName = cleanFileName(fileName);
+    if(cleanName !== 'index') {
+        const moduleRouter = await import(`./${cleanName}`);
+        router.use(`/${cleanName}`, moduleRouter.router);
+    }
+})
+
+export { router }
