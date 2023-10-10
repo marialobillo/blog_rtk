ba9d5e7eae43b529ef1cc8a34f182f46b9838c3d
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-10-10 12:32:07 +0200

    feat: add password handle using bcryptsjs

---
+++ src/utils/password.handle.ts
@@ -0,0 +1,18 @@
+import { hash, compare } from 'bcryptjs'
+
+const SALT_ROUNDS = 10
+
+const encryptPassword = async (password: string) => {
+    const hashedPassword = await hash(password, SALT_ROUNDS)
+    return hashedPassword
+}
+
+const verifyPassword = async (password: string, hashedPassword: string) => {
+    const isPasswordCorrect = await compare(password, hashedPassword)
+    return isPasswordCorrect
+}
+
+export {
+    encryptPassword,
+    verifyPassword
+}
