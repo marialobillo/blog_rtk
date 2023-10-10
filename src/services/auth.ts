7e5d801b70778b06f7451cc2b4b8edfe9d5cdedf
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2023-10-10 13:23:04 +0200

    fix: remove extra hash imported that is not needed

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,7 +1,7 @@
 import { IAuthor } from "../interfaces/author.interface"
 import { IAuth } from "../interfaces/auth.interface"
 import AuthorModel from "../models/Author"
-import { compare, hash } from 'bcryptjs';
+import { compare } from 'bcryptjs';
 import { encryptPassword } from "../utils/password.handle"
 import { generateToken } from "../utils/jwt.handle"
 
